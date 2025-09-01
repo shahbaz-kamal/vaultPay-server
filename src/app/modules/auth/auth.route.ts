b107323d12fa757d5a27d28e417e639db426992f
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-09-01 21:53:19 +0600

    google authentication completed

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,7 +1,8 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { AuthControllers } from "./auth.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
+import passport from "passport";
 
 const router = Router();
 
@@ -12,6 +13,22 @@ router.post(
   "/reset-password",
   checkAuth(...Object.values(Role)),
   AuthControllers.resetPassword
+);
+
+router.get(
+  "/google",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const redirect = req?.query?.redirect || "/";
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+      state: redirect as string,
+    })(req, res, next);
+  }
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  AuthControllers.googleCallbackController
 );
 // router.post("/get-all-users",A)
 
